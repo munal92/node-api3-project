c534fd649af5a6c39391a53f482c2bcee0c22ecc
unknown <m_fattih@hotmail.com>
2020-05-13 10:32:11 -0700

    mvp

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code away!
+const server = require("./server.js");
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+
+    console.log( `SERVER RUNNING ON ${PORT}`);
+})
--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const timestamp = require('time-stamp');
+
+const userRouter = require('./users/userRouter.js')
+
+
+
 
 const server = express();
+server.use(express.json())
+server.use(logger);
+
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+
+
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(req.method , req.url, timestamp('YYYY/MM/DD - HH:mm:ss:mm'));
+  
+ next();
+}
 
 module.exports = server;
